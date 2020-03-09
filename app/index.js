7fc6421e27c61b07a921b62da2ffb74a54364952
Andie Swift <andie.swift5@gmail.com>
2020-03-09 15:25:15 -0700

    initialization of the fitbit clockface app

---
+++ app/index.js
@@ -0,0 +1,43 @@
+import clock from "clock";
+import document from "document";
+import { preferences } from "user-settings";
+import * as util from "../common/utils";
+import { HeartRateSensor } from "heart-rate";
+
+// Update the clock every minute
+clock.granularity = "minutes";
+
+// Get a handle on the <text> element
+const myLabel = document.getElementById("myLabel");
+
+// Update the <text> element every tick with the current time
+clock.ontick = (evt) => {
+  let today = evt.date;
+  let hours = today.getHours();
+  let hBpm = document.getElementById("heartRate");
+
+  var hrm = new HeartRateSensor();
+  hrm.onreading = function () {
+
+    //hBpm.text = hrm.heartRate + " BPM";
+
+    //console.log("Current heart rate: " + hrm.heartRate);
+
+  }
+
+
+
+  hrm.start();
+
+
+
+  if (preferences.clockDisplay === "12h") {
+    // 12h format
+    hours = hours % 12 || 12;
+  } else {
+    // 24h format
+    hours = util.zeroPad(hours);
+  }
+  let mins = util.zeroPad(today.getMinutes());
+  myLabel.text = `${hours}:${mins}`;
+}
---
+++ common/utils.js
@@ -0,0 +1,10 @@
+import { HeartRateSensor } from "heart-rate";
+// Add zero in front of numbers < 10
+export function zeroPad(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+//HEART RATE
