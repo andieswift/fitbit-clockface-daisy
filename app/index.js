8117e442c2b13cb0bde3ba638343d7b04aef976d
Andie Swift <andie.swift5@gmail.com>
2020-03-12 18:40:21 -0700

    added the day of the week and the date to the top of the screen

--- app/index.js
+++ app/index.js
@@ -6,17 +6,29 @@ import { battery } from "power";
 import userActivity from "user-activity";
 import { HeartRateSensor } from "heart-rate";
 
+
 // SELECTORS
 const timeLabel = document.getElementById("timeLabel");
 const batteryLabel = document.getElementById("batteryLabel");
 const stepsLabel = document.getElementById("stepsLabel");
 const heartRateLabel = document.getElementById("heartRateLabel");
+const dateLabel = document.getElementById("dateLabel");
+const dayLabel = document.getElementById("dayLabel");
 
 //TIME
 clock.granularity = "minutes";
 clock.ontick = (evt) => {
   let today = evt.date;
   let hours = today.getHours();
+  let day = today.getDay();
+  let date = today.getDate();
+  let month = today.getMonth();
+
+  let dispDay = getDayName(day);
+  let dispMonth = getMonthName(month);
+
+  dateLabel.text = `${date} ${dispMonth}`;
+  dayLabel.text = dispDay;
   if (preferences.clockDisplay === "12h") {
     // 12h format
     hours = hours % 12 || 12;
@@ -43,3 +55,16 @@ hrm.onreading = function () {
   heartRateLabel.text = `${hrm.heartRate}`
 }
 hrm.start();
+
+//TODAY
+
+
+function getDayName(day) {
+  let days = ['SUN', 'MON', 'TUES', 'WED', 'THURS', 'FRI', 'SAT'];
+  return days[day];
+}
+
+function getMonthName(month) {
+  let months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+  return months[month];
+}
