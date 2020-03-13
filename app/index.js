08e010ebb1a4ed9a390abe85161b5ccaf845cb2a
Andie Swift <andie.swift5@gmail.com>
2020-03-12 17:46:33 -0700

    added battery to display

--- app/index.js
+++ app/index.js
@@ -2,35 +2,17 @@ import clock from "clock";
 import document from "document";
 import { preferences } from "user-settings";
 import * as util from "../common/utils";
-import { HeartRateSensor } from "heart-rate";
+import { battery } from "power";
 
-// Update the clock every minute
+// SELECTORS
+const timeLabel = document.getElementById("timeLabel");
+const batteryLabel = document.getElementById("batteryLabel");
+
+//TIME
 clock.granularity = "minutes";
-
-// Get a handle on the <text> element
-const myLabel = document.getElementById("myLabel");
-
-// Update the <text> element every tick with the current time
 clock.ontick = (evt) => {
   let today = evt.date;
   let hours = today.getHours();
-  let hBpm = document.getElementById("heartRate");
-
-  var hrm = new HeartRateSensor();
-  hrm.onreading = function () {
-
-    //hBpm.text = hrm.heartRate + " BPM";
-
-    //console.log("Current heart rate: " + hrm.heartRate);
-
-  }
-
-
-
-  hrm.start();
-
-
-
   if (preferences.clockDisplay === "12h") {
     // 12h format
     hours = hours % 12 || 12;
@@ -39,5 +21,9 @@ clock.ontick = (evt) => {
     hours = util.zeroPad(hours);
   }
   let mins = util.zeroPad(today.getMinutes());
-  myLabel.text = `${hours}:${mins}`;
+  timeLabel.text = `${hours}:${mins}`;
 }
+
+//BATTERY
+let batteryValue = battery.chargeLevel;
+batteryLabel.text = `${batteryValue}%`;
--- common/utils.js
+++ common/utils.js
@@ -6,5 +6,3 @@ export function zeroPad(i) {
   }
   return i;
 }
-
-//HEART RATE
